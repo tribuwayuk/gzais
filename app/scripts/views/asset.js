a8b2e56a57d84c60a9e0a4a75509b6a4f08512f7
Elizar Pepino <jupenz@gmail.com>
2013-12-05 01:45:41 +0800

    refactored assets views, model, and collection

--- app/scripts/views/asset.js
+++ app/scripts/views/asset.js
@@ -9,66 +9,19 @@ define([
   'use strict';
 
   var AssetView = Backbone.View.extend({
-    name_num_chars: 30,
-    desc_num_chars: 160,
-    type_num_chars: 30,
-    serial_num_chars: 30,
-    supplier_num_chars: 160,
-    reason_num_chars: 160,
+
+    className: 'asset-entry',
+
+    tagName: 'tr',
 
     template: JST['app/scripts/templates/asset.ejs'],
 
-    events: {
-      'click .btn-add': function() {
-        alert('aaaa');
-      }
-    },
-
-    saveAsset: function(e) {
-      e.preventDefault();
-      console.log('Asset Saved!');
-    },
-
-    validateNameChars: function(str) {
-      if (str.length > this.name_num_chars)
-        return false;
-      else
-        return true;
-    },
-
-    validateDescriptionChars: function(str) {
-      if (str.length > this.desc_num_chars)
-        return false;
-      else
-        return true;
-    },
-
-    validateTypeChars: function(str) {
-      if (str.length > this.type_num_chars)
-        return false;
-      else
-        return true;
-    },
-
-    validateSerialChars: function(str) {
-      if (str.length > this.serial_num_chars)
-        return false;
-      else
-        return true;
-    },
-
-    validateSupplierChars: function(str) {
-      if (str.length > this.supplier_num_chars)
-        return false;
-      else
-        return true;
-    },
-
-    validateReasonChars: function(str) {
-      if (str.length > this.reason_num_chars)
-        return false;
-      else
-        return true;
+    render: function() {
+      var self = this;
+      self.$el.html(self.template({
+        model: self.model
+      }));
+      return self;
     }
 
   });
--- app/scripts/views/employee.js
+++ app/scripts/views/employee.js
@@ -1,51 +1,51 @@
 /*global define*/
 
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'templates',
+  'jquery',
+  'underscore',
+  'backbone',
+  'templates',
 ], function($, _, Backbone, JST) {
-    'use strict';
+  'use strict';
 
-    var EmployeeView = Backbone.View.extend({
+  var EmployeeView = Backbone.View.extend({
 
-        className: 'employee-entry',
+    className: 'employee-entry',
 
-        tagName: 'tr',
+    tagName: 'tr',
 
-        template: JST['app/scripts/templates/employee.ejs'],
-	editTemplate: JST['app/scripts/templates/employee-edit.ejs'],
+    template: JST['app/scripts/templates/employee.ejs'],
+    editTemplate: JST['app/scripts/templates/employee-edit.ejs'],
 
-        events: {
-            'click .deleteEmployee': 'deleteList',
-	    'click .editEmployee': 'displayEditForm'
-	},
+    events: {
+      'click .deleteEmployee': 'deleteList',
+      'click .editEmployee': 'displayEditForm'
+    },
 
-	displayEditForm: function(e) {
-	    e.preventDefault();
-	    var self = this;
-	    $('#edit-modal').empty();
-	    $('#edit-modal').append(self.editTemplate({
-		model: this.model
-	    }));
-        },
+    displayEditForm: function(e) {
+      e.preventDefault();
+      var self = this;
+      $('#edit-modal').empty();
+      $('#edit-modal').append(self.editTemplate({
+        model: this.model
+      }));
+    },
 
-	deleteList: function() {
-	    // for implementation to delete data from db.
+    deleteList: function() {
+      // for implementation to delete data from db.
 
-	    // delete model.
-	    this.remove();
-        },
-        render: function() {
-            var self = this;
-            self.$el.html(self.template({
-                model: self.model
-            }));
-            return self;
-        }
-    });
+      // delete model.
+      this.remove();
+    },
+    render: function() {
+      var self = this;
+      self.$el.html(self.template({
+        model: self.model
+      }));
+      return self;
+    }
+  });
 
 
-    return EmployeeView;
+  return EmployeeView;
 });
