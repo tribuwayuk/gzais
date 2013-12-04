29ea3c789fc321e3ed73da3f884479045b43525a
Elizar Pepino <jupenz@gmail.com>
2013-12-05 02:50:06 +0800

    Fixed deletion method in asset model view and refactored employee deletion logic

--- app/scripts/views/asset.js
+++ app/scripts/views/asset.js
@@ -16,12 +16,36 @@ define([
 
     template: JST['app/scripts/templates/asset.ejs'],
 
+    events: {
+      'click .delete-asset': 'deleteAsset',
+      'click .edit-asset': 'displayEditForm'
+    },
+
+    initialize: function() {
+
+      var self = this;
+      // Proper way to handle deletion through events
+      self.listenTo(self.model, 'remove', function(index) {
+        return self.remove();
+      });
+
+    },
+
     render: function() {
       var self = this;
       self.$el.html(self.template({
         model: self.model
       }));
       return self;
+    },
+
+    deleteAsset: function() {
+      var self = this;
+      // for implementation to delete data from db.
+      if (confirm('Delete '+self.model.get('asset_name')+' ?')) {
+        // delete model.
+        return self.model.collection.remove(self.model);
+      }
     }
 
   });
