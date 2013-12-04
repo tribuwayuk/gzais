823670a19715135ac2b48c4fbb33b657f02183d7
Rohit Daryanani <rohit.daryanani@globalzeal.net>
2013-12-04 16:17:29 +0800

    added form validations for new employees

--- app/scripts/models/employee.js
+++ app/scripts/models/employee.js
@@ -1,12 +1,21 @@
 /*global define*/
 define([
-  'underscore',
-  'backbone'
+    'underscore',
+    'backbone'
 ], function(_, Backbone) {
-  'use strict';
+    'use strict';
 
-  var EmployeeModel = Backbone.Model.extend({
-  });
+    var EmployeeModel = Backbone.Model.extend({
+        validate: function(attrs) {
+            if (!attrs.email) {
+                alert( 'Please fill email field.');
+            }
 
-  return EmployeeModel;
+            if (!attrs.first_name) {
+                return 'Please fill feedback field.';
+            }
+        }
+    });
+
+    return EmployeeModel;
 });
--- app/scripts/views/employees.js
+++ app/scripts/views/employees.js
@@ -6,7 +6,7 @@ define([
     'backbone',
     'templates',
     'EmployeeView'
-], function ($, _, Backbone, JST, EmployeeView) {
+], function($, _, Backbone, JST, EmployeeView) {
     'use strict';
 
     var EmployeesView = Backbone.View.extend({
@@ -14,47 +14,67 @@ define([
         template: JST['app/scripts/templates/employees.ejs'],
 
         events: {
-          'submit form': 'newEmployee',
-          'click btn-add': 'newEmployee'
+            'submit form': 'newEmployee',
+            'click btn-add': 'newEmployee'
         },
-
+        errorFields: [],
         newEmployee: function(e) {
-          e.preventDefault();
-          var self        = this,
-              form        = e.currentTarget,
-              newEmployee = {};
+            e.preventDefault();
+            var self = this,
+                form = e.currentTarget,
+                newEmployee = {};
 
-          newEmployee.first_name    = form.first_name.value;
-          newEmployee.middle_name   = form.middle_name.value;
-          newEmployee.last_name     = form.last_name.value;
-          newEmployee.email         = form.email.value;
-          newEmployee.gender        = form.gender.value;
-          newEmployee.date_of_birth = form.date_of_birth.value;
-          newEmployee.date_employed = form.date_employed.value;
-          newEmployee.user_role     = form.user_role.value;
 
-          self.collection.add(newEmployee);
-          
-          $('#add-modal').modal('hide');
+            newEmployee.first_name = self.fieldValidation(form.first_name, /^.{2,}$/);
+            newEmployee.middle_name = self.fieldValidation(form.middle_name, /^.{2,}$/);
+            newEmployee.last_name = self.fieldValidation(form.last_name, /^.{2,}$/);
+            newEmployee.email = self.fieldValidation(form.email, /^[a-z0-9._%\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$/);
+            newEmployee.gender = form.gender.value;
+            newEmployee.date_of_birth = self.fieldValidation(form.date_of_birth, /^.{2,}$/);
+            newEmployee.date_employed = self.fieldValidation(form.date_employed, /^.{2,}$/);
+            newEmployee.user_role = form.user_role.value;
+
+            if (self.errorFields.length === 0) {
+                self.collection.add(newEmployee);
+                $('#add-modal').modal('hide');
+            } else {
+                
+                self.errorFields = [];
+            }
 
         },
 
         render: function() {
-          var self = this;
-          self.$el.html(self.template());
-          return self;
+            var self = this;
+            self.$el.html(self.template());
+            return self;
         },
 
         initialize: function() {
-          var self = this;
-          self.listenTo(self.collection, 'add', self.onAdd);
-          self.collection.fetch();
+            var self = this;
+            self.listenTo(self.collection, 'add', self.onAdd);
+            self.collection.fetch();
         },
 
         onAdd: function(model) {
-          var self = this;
-          var employee = new EmployeeView({model: model});
-          $('tbody.employees-list').prepend(employee.render().el);
+            var self = this;
+            var employee = new EmployeeView({
+                model: model
+            });
+            $('tbody.employees-list').prepend(employee.render().el);
+        },
+
+        fieldValidation: function(field, regexp) {
+            console.log(field.value.match(regexp));
+            $(field).removeClass('error');
+            if (field.value.match(regexp) !== null) {
+                $(field).parent().removeClass('has-error');
+                return field.value;
+            } else {                
+                this.errorFields.pop(field.id);
+                this.errorFields.push(field.id);
+                return $(field).parent().addClass('has-error');
+            }
         }
 
     });
