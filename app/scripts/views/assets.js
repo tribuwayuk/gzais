2aeeec4ab4fcc14ff571036347240300747b5d38
Rey Philip Regis <rey.regis@globalzeal.net>
2013-12-05 15:17:04 +0800

    fixed invalid characters input on textfields, it will now notify user that the user inputted invalid characters in the form

--- app/scripts/views/assets.js
+++ app/scripts/views/assets.js
@@ -2,115 +2,131 @@
 
 define([
 
-  'jquery',
-  'underscore',
-  'backbone',
-  'templates',
-  'AssetView'
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates',
+    'AssetView'
 ], function($, _, Backbone, JST, AssetView) {
-  'use strict';
+    'use strict';
 
-  var AssetsView = Backbone.View.extend({
+    var AssetsView = Backbone.View.extend({
 
-    template: JST['app/scripts/templates/assets.ejs'],
+        template: JST['app/scripts/templates/assets.ejs'],
 
-    events: {
-      'submit form': 'newAsset',
-      'submit form#editForm': 'editAsset',
-      'click btn-add': 'newAsset'
-    },
+        events: {
+            'submit form': 'newAsset',
+            'submit form#editForm': 'editAsset',
+            'click btn-add': 'newAsset',
+            'focusout .form-control' : 'isValid'
+        },
 
-    errorFields: [],
+        errorFields: [],
 
-    newAsset: function(e) {
+        newAsset: function(e) {
 
-      e.preventDefault();
+            e.preventDefault();
 
-      var self = this,
-        form = e.currentTarget,
-        newAsset = {};
+            var self = this,
+                form = e.currentTarget,
+                newAsset = {};
 
 
-      newAsset.asset_name = self.fieldValidation(form.asset_name, /^.{2,}$/);
-      newAsset.asset_type = self.fieldValidation(form.asset_type, /^.{2,}$/);
-      newAsset.date_purchased = self.fieldValidation(form.date_purchased, /^.{2,}$/);
-      newAsset.status = self.fieldValidation(form.status, /^(working|defective)$/);
-      newAsset.serial_number = self.fieldValidation(form.serial_number, /^.{5,}$/);
-      newAsset.supplier = self.fieldValidation(form.supplier, /^.{2,}$/);
-      newAsset.reason = form.reason.value;
-      newAsset.asset_description = self.fieldValidation(form.asset_description, /^.{2,}$/);
+            newAsset.asset_name = self.fieldValidation(form.asset_name, /^.{2,}$/);
+            newAsset.asset_type = self.fieldValidation(form.asset_type, /^.{2,}$/);
+            newAsset.date_purchased = self.fieldValidation(form.date_purchased, /^.{2,}$/);
+            newAsset.status = self.fieldValidation(form.status, /^(working|defective)$/);
+            newAsset.serial_number = self.fieldValidation(form.serial_number, /^.{5,}$/);
+            newAsset.supplier = self.fieldValidation(form.supplier, /^.{2,}$/);
+            newAsset.reason = form.reason.value;
+            newAsset.asset_description = self.fieldValidation(form.asset_description, /^.{2,}$/);
 
-      if (self.errorFields.length === 0) {
-        self.ajaxRequest(form, newAsset);
-      } else {
-        self.errorFields = [];
-      }
+            if (self.errorFields.length === 0) {
+                self.ajaxRequest(form, newAsset);
+            } else {
+                self.errorFields = [];
+            }
 
-    },
+        },
 
-    ajaxRequest: function(form, data) {
-      var self = this;
-      $.post(self.collection.url, data).done(function(result) {
-        if (!result.errors) {
-          self.collection.add(result);
-          form.reset();
-          $('#add-modal').modal('hide');
+        ajaxRequest: function(form, data) {
+            var self = this;
+            $.post(self.collection.url, data).done(function(result) {
+                if (!result.errors) {
+                    self.collection.add(result);
+                    form.reset();
+                    $('#add-modal').modal('hide');
+                }
+            });
+        },
+
+        editAsset: function(e) {
+            e.preventDefault();
+            var self = this,
+                form = e.currentTarget,
+                editAsset = {};
+
+            editAsset.asset_name = form.asset_name.value;
+            editAsset.asset_type = form.asset_type.value;
+            editAsset.date_purchased = form.date_purchased.value;
+            editAsset.status = form.status.value;
+            editAsset.serial_number = form.serial_number.value;
+            editAsset.supplier = form.supplier.value;
+            editAsset.reason = form.reason.value;
+            editAsset.asset_description = form.asset_description.value;
+
+
+            // to do: implement update collection.
+        },
+
+        render: function() {
+            var self = this;
+            self.$el.html(self.template({}));
+            return self;
+        },
+
+        initialize: function() {
+            var self = this;
+            self.listenTo(self.collection, 'add', self.onAdd);
+            self.collection.fetch();
+        },
+
+        onAdd: function(model) {
+            var self = this;
+            var asset = new AssetView({
+                model: model
+            });
+            $('tbody.assets-list').prepend(asset.render().el);
+        },
+
+        fieldValidation: function(field, regexp) {
+            $(field).removeClass('error');
+            if (field.value.match(regexp) !== null) {
+                $(field).parent().removeClass('has-error');
+                return field.value;
+            } else {
+                this.errorFields.pop(field.id);
+                this.errorFields.push(field.id);
+                return $(field).parent().addClass('has-error');
+            }
+        },
+
+        isValid : function(ev) {
+            var id = '#' + ev.currentTarget.id;
+            var str = ev.currentTarget.value;
+            var iChars = "~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
+
+            for (var i = 0; i < str.length; i++) {
+                if (iChars.indexOf(str.charAt(i)) != -1) {
+                    alert("File name has special characters ~`!#$%^&*+=-[]\\\';,/{}|\":<>? \nThese are not allowed\n");
+                    $(id).focus();
+                    return false;
+                }
+            }
+            return true;
         }
-      });
-    },
 
-    editAsset: function(e) {
-      e.preventDefault();
-      var self = this,
-        form = e.currentTarget,
-        editAsset = {};
+    });
 
-      editAsset.asset_name = form.asset_name.value;
-      editAsset.asset_type = form.asset_type.value;
-      editAsset.date_purchased = form.date_purchased.value;
-      editAsset.status = form.status.value;
-      editAsset.serial_number = form.serial_number.value;
-      editAsset.supplier = form.supplier.value;
-      editAsset.reason = form.reason.value;
-      editAsset.asset_description = form.asset_description.value;
-
-
-      // to do: implement update collection.
-    },
-
-    render: function() {
-      var self = this;
-      self.$el.html(self.template({}));
-      return self;
-    },
-
-    initialize: function() {
-      var self = this;
-      self.listenTo(self.collection, 'add', self.onAdd);
-      self.collection.fetch();
-    },
-
-    onAdd: function(model) {
-      var self = this;
-      var asset = new AssetView({
-        model: model
-      });
-      $('tbody.assets-list').prepend(asset.render().el);
-    },
-
-    fieldValidation: function(field, regexp) {
-      $(field).removeClass('error');
-      if (field.value.match(regexp) !== null) {
-        $(field).parent().removeClass('has-error');
-        return field.value;
-      } else {
-        this.errorFields.pop(field.id);
-        this.errorFields.push(field.id);
-        return $(field).parent().addClass('has-error');
-      }
-    }
-
-  });
-
-  return AssetsView;
+    return AssetsView;
 });
