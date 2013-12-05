e2d956314d29d6a71167754dbbef8b48bc6eadb7
Alexjander Bacalso <janderbacalso@gmail.com>
2013-12-05 10:48:18 +0800

    persist saving employee to db.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -96,6 +96,23 @@ require([
     //window.location = window.location.hash.substring(1);
   //}
 
+  // serialize form input
+  $.fn.serializeObject = function() {
+      var o = {};
+      var a = this.serializeArray();
+      $.each(a, function() {
+	  if (o[this.name] !== undefined) {
+	      if (!o[this.name].push) {
+		  o[this.name] = [o[this.name]];
+	      }
+	      o[this.name].push(this.value || '');
+	  } else {
+	      o[this.name] = this.value || '';
+	  }
+      });
+      return o;
+  };
+
   // Init Backbone.history
   Backbone.history.start({
     pushState: true
--- app/scripts/views/employees.js
+++ app/scripts/views/employees.js
@@ -2,104 +2,113 @@
 
 define([
 
-  'jquery',
-  'underscore',
-  'backbone',
-  'templates',
-  'EmployeeView'
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates',
+    'EmployeeView'
 ], function($, _, Backbone, JST, EmployeeView) {
-  'use strict';
+    'use strict';
 
-  var EmployeesView = Backbone.View.extend({
+    var EmployeesView = Backbone.View.extend({
 
-    template: JST['app/scripts/templates/employees.ejs'],
+	template: JST['app/scripts/templates/employees.ejs'],
 
-    events: {
-      'submit form#addForm': 'newEmployee',
-      'submit form#editForm': 'editEmployee',
-      'click btn-add': 'newEmployee'
-    },
-    errorFields: [],
-    newEmployee: function(e) {
-      e.preventDefault();
-      var self = this,
-        form = e.currentTarget,
-        newEmployee = {};
+	events: {
+	    'submit form#addForm': 'newEmployee',
+	    'submit form#editForm': 'editEmployee',
+	    'click btn-add': 'newEmployee'
+	},
+	errorFields: [],
+	newEmployee: function(e) {
+	    e.preventDefault();
+	    var self = this,
+		form = e.currentTarget,
+		newEmployee = {};
+
+	    newEmployee.first_name = self.fieldValidation(form.first_name, /^.{2,}$/);
+	    newEmployee.middle_name = self.fieldValidation(form.middle_name, /^.{2,}$/);
+	    newEmployee.first_name = self.fieldValidation(form.first_name, /^.{2,}$/);
+	    newEmployee.last_name = self.fieldValidation(form.last_name, /^.{2,}$/);
+	    newEmployee.address = self.fieldValidation(form.address, /^.{2,}$/);
+	    newEmployee.email = self.fieldValidation(form.email, /^[a-z0-9._%\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$/);
+	    newEmployee.gender = form.gender.value;
+	    newEmployee.date_of_birth = self.fieldValidation(form.date_of_birth, /^.{2,}$/);
+	    newEmployee.date_employed = self.fieldValidation(form.date_employed, /^.{2,}$/);
+	    newEmployee.user_role = form.user_role.value;
+
+	    if (self.errorFields.length === 0) {
+		console.log(form);
+		self.ajaxRequestSave(form, newEmployee);
+		/*self.model.save($(form).serializeObject(), {
+		    success: function(inputData) {
+			alert('Save successfully!');
+		    }
+		});*/
+
+		//self.collection.add(newEmploye\e);
+		//form.reset();
+	    } else {
+
+		self.errorFields = [];
+	    }
+
+	},
+	editEmployee: function(e) {
+	    e.preventDefault();
+	    var self = this,
+		form = e.currentTarget,
+		editEmployee = {};
+
+	    editEmployee.first_name = form.first_name.value;
+	    editEmployee.middle_name = form.middle_name.value;
+	    editEmployee.last_name = form.last_name.value;
+	    editEmployee.email = form.email.value;
+	    editEmployee.gender = form.gender.value;
+	    editEmployee.date_of_birth = form.date_of_birth.value;
+	    editEmployee.date_employed = form.date_employed.value;
+	    editEmployee.user_role = form.user_role.value;
 
 
-      newEmployee.first_name = self.fieldValidation(form.first_name, /^.{2,}$/);
-      newEmployee.middle_name = self.fieldValidation(form.middle_name, /^.{2,}$/);
-      newEmployee.first_name = self.fieldValidation(form.first_name, /^.{2,}$/);
-      newEmployee.last_name = self.fieldValidation(form.last_name, /^.{2,}$/);
-      newEmployee.address = self.fieldValidation(form.address, /^.{2,}$/);
-      newEmployee.email = self.fieldValidation(form.email, /^[a-z0-9._%\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$/);
-      newEmployee.gender = form.gender.value;
-      newEmployee.date_of_birth = self.fieldValidation(form.date_of_birth, /^.{2,}$/);
-      newEmployee.date_employed = self.fieldValidation(form.date_employed, /^.{2,}$/);
-      newEmployee.user_role = form.user_role.value;
+	    // to do: implement update collection.
+	},
 
-      if (self.errorFields.length === 0) {
-        self.collection.add(newEmployee);
-        form.reset();
-        $('#add-modal').modal('hide');
-      } else {
+	render: function() {
+	    var self = this;
+	    self.$el.html(self.template());
+	    return self;
+	},
 
-        self.errorFields = [];
-      }
+	initialize: function() {
+	    var self = this;
+	    self.listenTo(self.collection, 'add', self.onAdd);
+	    self.collection.fetch();
+	},
+	ajaxRequestSave: function() {
+	  con
+	  $('#add-modal').modal('hide');
+	},
+	onAdd: function(model) {
+	    var self = this;
+	    var employee = new EmployeeView({
+		model: model
+	    });
+	    $('tbody.employees-list').prepend(employee.render().el);
+	},
 
-    },
-    editEmployee: function(e) {
-      e.preventDefault();
-      var self = this,
-        form = e.currentTarget,
-        editEmployee = {};
+	fieldValidation: function(field, regexp) {
+	    $(field).removeClass('error');
+	    if (field.value.match(regexp) !== null) {
+		$(field).parent().removeClass('has-error');
+		return field.value;
+	    } else {
+		this.errorFields.pop(field.id);
+		this.errorFields.push(field.id);
+		return $(field).parent().addClass('has-error');
+	    }
+	}
 
-      editEmployee.first_name = form.first_name.value;
-      editEmployee.middle_name = form.middle_name.value;
-      editEmployee.last_name = form.last_name.value;
-      editEmployee.email = form.email.value;
-      editEmployee.gender = form.gender.value;
-      editEmployee.date_of_birth = form.date_of_birth.value;
-      editEmployee.date_employed = form.date_employed.value;
-      editEmployee.user_role = form.user_role.value;
+    });
 
-
-      // to do: implement update collection.
-    },
-
-    render: function() {
-      var self = this;
-      self.$el.html(self.template());
-      return self;
-    },
-
-    initialize: function() {
-      var self = this;
-      self.listenTo(self.collection, 'add', self.onAdd);
-      self.collection.fetch();
-    },
-
-    onAdd: function(model) {
-      var self = this;
-      var employee = new EmployeeView({
-        model: model
-      });
-      $('tbody.employees-list').prepend(employee.render().el);
-    },
-
-    fieldValidation: function(field, regexp) {
-      $(field).removeClass('error');
-      if (field.value.match(regexp) !== null) {
-        $(field).parent().removeClass('has-error');
-        return field.value;
-      } else {
-        this.errorFields.pop(field.id);
-        this.errorFields.push(field.id);
-        return $(field).parent().addClass('has-error');
-      }
-    }
-
-  });
-
-  return EmployeesView;
+    return EmployeesView;
 });
