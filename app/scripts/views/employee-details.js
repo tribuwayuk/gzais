605f813ca8f5ce12dbdeab806c17493580716a26
Johmel Rey Pintor <jahmgwapo@me.com>
2013-12-11 14:39:13 +0800

    Added employee-details.js views for employee details.
    Modified the following:
    	main.js under scripts
    	employee.js under models
    	app.js under routes
    	employee-details.ejs and employee.ejs under templates
    	assets.js under views
    to work with employee details.

---
+++ app/scripts/views/employee-details.js
@@ -0,0 +1,29 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    var EmployeeDetailsView = Backbone.View.extend({
+
+        template: JST['app/scripts/templates/employee-details.ejs'],
+
+        className: 'employee-details',
+
+        render: function(model) {
+          var self = this;
+          self.$el.html(self.template({model: self.model}));
+          return self;
+        },
+
+        initialize: function() {
+        }
+
+    });
+
+    return EmployeeDetailsView;
+});
