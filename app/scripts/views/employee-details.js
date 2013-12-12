d0a779f8ca6008b8c775820caeb078541a56f544
Johmel Rey Pintor <jahmgwapo@me.com>
2013-12-12 15:22:14 +0800

    ID1500 Modified the following files:
    	both employee-details.ejs and employee-edit.ejs under templates
    	both employee-details.js and employee.js under views
    
    to work with EDIT user under View User

--- app/scripts/views/employee-details.js
+++ app/scripts/views/employee-details.js
@@ -14,15 +14,80 @@ define( [
 
         className: 'employee-details',
 
-        render: function( model ) {
+		errorFields: [ ],
+
+		events: {
+            'submit form#edit-form': 'updateEmployee'
+		},
+
+        initialize: function( ) {},
+
+		updateEmployee: function( e ) {
+            e.preventDefault( );
+
+            console.log(e);
+
             var self = this;
+            var form = e.currentTarget;
+            var data = {};
+
+            data.first_name    = self.fieldValidation( form.first_name, /^[a-zA-Z\s]{1,30}$/ );
+            data.middle_name   = self.fieldValidation( form.middle_name, /^[a-zA-Z\s]{1,30}$/ );
+            data.last_name     = self.fieldValidation( form.last_name, /^[a-zA-Z\s]{1,30}$/ );
+            data.address       = self.fieldValidation( form.address, /^.{2,60}$/ );
+            data.email         = self.fieldValidation( form.email, /^[a-z0-9._%\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$/ );
+            data.gender        = self.fieldValidation( form.gender, /^(male|female)$/ );
+            data.date_of_birth = self.fieldValidation( form.date_of_birth, /^\d{2}\/\d{2}\/\d{4}$/ );
+            data.date_employed = self.fieldValidation( form.date_employed, /^\d{2}\/\d{2}\/\d{4}$/ );
+            data.user_role     = self.fieldValidation( form.user_role, /^(admin|custodian|employee)$/ );
+
+            if ( self.errorFields.length === 0 ) {
+                $( 'input, button, option, textarea' ).prop( 'disabled', true );
+                $( '.btns' ).addClass( 'loading' );
+
+                self.model.save( data, {
+                    success: function( ) {
+                        $( '#edit-modal' ).modal( 'hide' );
+                        $( 'input, button, option, textarea' ).prop( 'disabled', false );
+                        $( '.btns' ).removeClass( 'loading' );
+                        self.render( );
+                    },
+                    error: function( ) {
+                        $( '#edit-modal' ).modal( 'hide' );
+                    }
+                } );
+
+            } else {
+                self.errorFields = [ ];
+            }
+
+        },
+
+        fieldValidation: function( field, regexp ) {
+
+            $( field ).removeClass( 'error' );
+
+            if ( field.value.match( regexp ) !== null ) {
+                $( field ).parent( ).removeClass( 'has-error' );
+                return field.value;
+            } else {
+                this.errorFields.pop( field.id );
+                this.errorFields.push( field.id );
+                return $( field ).parent( ).addClass( 'has-error' );
+            }
+
+        },
+
+        render: function( ) {
+
+            var self = this;
+
             self.$el.html( self.template( {
                 model: self.model
             } ) );
-            return self;
-        },
 
-        initialize: function( ) {}
+            return self;
+        }
 
     } );
 
