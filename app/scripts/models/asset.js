4bfa8e4ac4f4e21dcfa836e32187a2c74256db2a
Elizar Pepino <jupenz@gmail.com>
2013-12-12 09:42:12 +0800

    fixed date issue on asset

--- app/scripts/models/asset.js
+++ app/scripts/models/asset.js
@@ -14,8 +14,8 @@ define([
     getDatePurchased: function() {
 
       var date   = new Date(this.get('date_purchased')),
-          _month = (date.getMonth() + 1 < 10) ? '0' + date.getMonth() : '' + date.getMonth(),
-          _date  = (date.getDate() + 1 < 10) ? '0' + date.getDate() : '' + date.getDate(),
+          _month = (date.getMonth() + 1 < 10) ? '0' + date.getMonth() + 1: '' + date.getMonth() + 1,
+          _date  = (date.getDate() + 1 < 10) ? '0' + date.getDate() + 1: '' + date.getDate() + 1,
           _year  = date.getFullYear();
 
       return _month + '/' + _date + '/' + _year;
