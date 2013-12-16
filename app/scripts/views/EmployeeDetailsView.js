f6511cd41247e4f0bbc9d6df433abc1a91b3f78f
Johmel Rey Pintor <jahmgwapo@me.com>
2013-12-16 10:27:18 +0800

    Modified the following files 	Removed console.log on EmployeeDetailsView and EmployeeView under 	views 	Complete the changes to work with edit

--- app/scripts/views/EmployeeDetailsView.js
+++ app/scripts/views/EmployeeDetailsView.js
@@ -17,8 +17,7 @@ define( [
             'submit form#edit-form': 'updateEmployee'
 		},
 
-		updateEmployee : function( e ) {
-
+        updateEmployee : function( e ) {
             e.preventDefault( );
 
             var self = this;
@@ -29,7 +28,7 @@ define( [
             data.middle_name   = self.fieldValidation( form.middle_name, /^[a-zA-Z\s]{1,30}$/ );
             data.last_name     = self.fieldValidation( form.last_name, /^[a-zA-Z\s]{1,30}$/ );
             data.address       = self.fieldValidation( form.address, /^.{2,60}$/ );
-            data.email         = self.fieldValidation( form.email, /^[a-z0-9._%\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$/ );
+            data.email         = self.fieldValidation( form.email, /^[a-z]+\.[a-z]+@globalzeal\.net$/ );
             data.gender        = self.fieldValidation( form.gender, /^(male|female)$/ );
             data.date_of_birth = self.fieldValidation( form.date_of_birth, /^\d{2}\/\d{2}\/\d{4}$/ );
             data.date_employed = self.fieldValidation( form.date_employed, /^\d{2}\/\d{2}\/\d{4}$/ );
@@ -40,18 +39,27 @@ define( [
                 $( 'input, button, option, textarea' ).prop( 'disabled', true );
                 $( '.btns' ).addClass( 'loading' );
 
-                self.model.save( data, {
-                    success: function( ) {
-                        $( '#edit-modal' ).modal( 'hide' );
-                        $( 'input, button, option, textarea' ).prop( 'disabled', false );
-                        $( '.btns' ).removeClass( 'loading' );
-                        self.render( );
-                    },
-                    error: function( res, err ) {
-                        if ( JSON.stringify( err ) && JSON.stringify( err ).match( /email/ ) ) {
+				self.model.save( data, {
+					wait: true,
+                    success: function( model, xhr, options) {
+						if ( JSON.stringify( xhr ) && JSON.stringify( xhr ).match( /email/ ) ) {
 							$( 'input, button, option' ).prop( 'disabled', false );
-							$( '.btns' ).removeClass( 'loading' );
+							$( '.btns' ).removeClass( 'error loading' );
 							$( form[ 'email' ] ).parent( ).addClass( 'has-error error' );
+							model.set(model.previousAttributes(),{silent: true});					// if error, reset modal value
+						} else {
+							$( '#edit-modal' ).modal( 'hide' );
+							$( 'input, button, option, textarea' ).prop( 'disabled', false );
+							$( '.btns' ).removeClass( 'loading' );
+							self.render( );
+						}
+                    },
+                    error: function( model, xhr, options) {
+						if ( JSON.stringify( xhr ) && JSON.stringify( xhr ).match( /email/ ) ) {
+							$( 'input, button, option' ).prop( 'disabled', false );
+							$( '.btns' ).removeClass( 'error loading' );
+							$( form[ 'email' ] ).parent( ).addClass( 'has-error error' );
+							model.set(model.previousAttributes(),{silent: true});					// if error, reset modal value
 						}
                     }
                 } );
--- app/scripts/views/EmployeeView.js
+++ app/scripts/views/EmployeeView.js
@@ -34,7 +34,7 @@ define( [
                     return self.remove( );
                 };
                 options.error = function( ) {
-                    console.log( 'error' );
+
                 };
 
                 self.model.collection.sync( 'delete', self.model, options );
@@ -76,7 +76,7 @@ define( [
                 'type'    : "POST",
                 'url'     : urlRoot,
                 'success' : function( ) {
-                    console.log( 'success!' );
+
                 },
             } );
 
@@ -112,7 +112,7 @@ define( [
             data.middle_name   = self.fieldValidation( form.middle_name, /^[a-zA-Z\s]{1,30}$/ );
             data.last_name     = self.fieldValidation( form.last_name, /^[a-zA-Z\s]{1,30}$/ );
             data.address       = self.fieldValidation( form.address, /^.{2,60}$/ );
-            data.email         = self.fieldValidation( form.email, /^[a-z0-9._%\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$/ );
+            data.email         = self.fieldValidation( form.email, /^[a-z]+\.[a-z]+@globalzeal\.net$/ );
             data.gender        = self.fieldValidation( form.gender, /^(male|female)$/ );
             data.date_of_birth = self.fieldValidation( form.date_of_birth, /^\d{2}\/\d{2}\/\d{4}$/ );
             data.date_employed = self.fieldValidation( form.date_employed, /^\d{2}\/\d{2}\/\d{4}$/ );
@@ -127,16 +127,15 @@ define( [
 					wait: true,
                     success: function( model, xhr, options) {
 						if ( JSON.stringify( xhr ) && JSON.stringify( xhr ).match( /email/ ) ) {
-							console.log(self.model.previousAttributes());
 							$( 'input, button, option' ).prop( 'disabled', false );
 							$( '.btns' ).removeClass( 'error loading' );
 							$( form[ 'email' ] ).parent( ).addClass( 'has-error error' );
 							model.set(model.previousAttributes(),{silent: true});					// if error, reset modal value
 						} else {
-							self.render( );
 							$( '#edit-modal' ).modal( 'hide' );
 							$( 'input, button, option, textarea' ).prop( 'disabled', false );
 							$( '.btns' ).removeClass( 'loading' );
+							self.render( );
 						}
                     },
                     error: function( model, xhr, options) {
