69b96f27ddde9127db3fd55eea2ea9695aec5625
Carter Binarao <carter.binarao@globalzeal.net>
2013-12-18 16:32:57 +0800

    fix edit button on view details. issue 166

--- app/scripts/views/AssetDetailsView.js
+++ app/scripts/views/AssetDetailsView.js
@@ -10,35 +10,50 @@ define( [
 
     var AssetDetailsView = Backbone.View.extend( {
 
-	template    : JST[ 'app/scripts/templates/asset-details.ejs' ],
-	className   : 'asset-details',
-	errorFields : [ ],
-	events      : {
-	    'submit form#edit-form' : 'updateAsset',
-	    'click .delete-asset'   : 'deleteAsset'
+        template: JST[ 'app/scripts/templates/asset-details.ejs' ],
+        editTemplate        : JST['app/scripts/templates/asset-edit.ejs'],
+        className: 'asset-details',
+        errorFields: [ ],
+        events: {
+            //'submit form#edit-form' : 'updateAsset',
+            'click .delete-asset'   : 'deleteAsset',
+            'click .edit-asset'		: 'displayEditForm'
         },
 
-	initialize: function ( ) {
+        initialize: function ( ) {
 
-	    var self = this;
+            var self = this;
 
-	    self.listenTo( self.model, 'remove', function ( index ) {
+            self.listenTo( self.model, 'remove', function ( index ) {
 
-		var options = options || {};
-		options.url = self.model.url( );
+                var options = options || {};
+                options.url = self.model.url( );
 
-		options.success = function ( ) {
-		    window.App.router.navigate( '/assets', true );
-		    return self.remove( );
-		};
-		options.error = function ( ) {
-		    console.error( 'error' );
-		};
+                options.success = function ( ) {
+                    window.App.router.navigate( '/assets', true );
+                    return self.remove( );
+                };
+                options.error = function ( ) {
+                    console.error( 'error' );
+                };
 
-		self.model.collection.sync( 'delete', self.model, options );
-	    } );
+                self.model.collection.sync( 'delete', self.model, options );
+            } );
 
-	},
+        },
+        displayEditForm: function( e ) {
+
+            var self = this;
+
+            $( '#edit-modal' ).empty( );
+            $( '#edit-modal' ).append( self.editTemplate( {
+                model: self.model
+            } ) );
+
+            $( '#edit-form' ).submit( self.updateAsset.bind( self ) );
+
+        },
+
 
         updateAsset: function ( e ) {
 
@@ -48,13 +63,13 @@ define( [
             var form = e.currentTarget;
             var data = {};
 
-	    data.asset_name        = self.fieldValidation( form.asset_name, /^[a-zA-Z0-9\.\-\,\''\s]{3,30}$/ );
-	    data.asset_type        = self.fieldValidation( form.asset_type, /^[a-zA-Z0-9\s]{3,30}$/ );
-	    data.date_purchased    = self.fieldValidation( form.date_purchased, /^\d{2}\/\d{2}\/\d{4}$/ );
-	    data.status            = self.fieldValidation( form.status, /^(working|defective)$/ );
-	    data.serial_number     = self.fieldValidation( form.serial_number, /^[a-zA-Z0-9-\s]{5,30}$/ );
-	    data.supplier          = self.fieldValidation( form.supplier, /^[a-zA-Z0-9\.\-\,\&\s]{5,160}$/ );
-	    data.reason            = self.fieldValidation( form.reason, /^.{5,160}$/ );
+            data.asset_name        = self.fieldValidation( form.asset_name, /^[a-zA-Z0-9\.\-\,\''\s]{3,30}$/ );
+            data.asset_type        = self.fieldValidation( form.asset_type, /^[a-zA-Z0-9\s]{3,30}$/ );
+            data.date_purchased    = self.fieldValidation( form.date_purchased, /^\d{2}\/\d{2}\/\d{4}$/ );
+            data.status            = self.fieldValidation( form.status, /^(working|defective)$/ );
+            data.serial_number     = self.fieldValidation( form.serial_number, /^[a-zA-Z0-9-\s]{5,30}$/ );
+            data.supplier          = self.fieldValidation( form.supplier, /^[a-zA-Z0-9\.\-\,\&\s]{5,160}$/ );
+            data.reason            = self.fieldValidation( form.reason, /^.{5,160}$/ );
             data.asset_description = self.fieldValidation( form.asset_description, /^.{5,160}$/ );
 
             if ( self.errorFields.length === 0 ) {
@@ -102,30 +117,30 @@ define( [
             return self;
         },
 
-	deleteAsset: function ( ) {
+        deleteAsset: function ( ) {
 
-	    var self = this;
-	    var bootbox = window.bootbox;
+            var self = this;
+            var bootbox = window.bootbox;
 
-	    bootbox.dialog( {
-		message : 'Are you sure you want to delete ' + self.model.get( 'asset_name' ) + ' ?',
-		title   : "Confirm Deletion",
-		buttons : {
-		    default: {
-			label     : " Cancel ",
-			className : "btn-default",
-			callback  : function ( ) {}
-		    },
-		    danger: {
-			label     : " Yes ",
-			className : "btn-danger",
-			callback  : function ( ) {
-			    return self.model.collection.remove( self.model );
-			}
-		    }
-		}
-	    } );
-	},
+            bootbox.dialog( {
+                message: 'Are you sure you want to delete ' + self.model.get( 'asset_name' ) + ' ?',
+                title: "Confirm Deletion",
+                buttons: {
+                    default: {
+                        label: " Cancel ",
+                        className: "btn-default",
+                        callback: function ( ) {}
+                    },
+                    danger: {
+                        label: " Yes ",
+                        className: "btn-danger",
+                        callback: function ( ) {
+                            return self.model.collection.remove( self.model );
+                        }
+                    }
+                }
+            } );
+        },
 
     } );
 
